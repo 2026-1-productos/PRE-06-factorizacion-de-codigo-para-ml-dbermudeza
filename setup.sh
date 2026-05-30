7350e587a8ee4df37566cb753d0899e3d3b1af39
dbermudeza <dbermudeza@unal.edu.co>
2026-05-30 09:25:05 -0500

    Add directory creation and estimator generation to setup

--- setup.sh
+++ setup.sh
@@ -3,3 +3,7 @@
 pip3 install --upgrade pip
 pip3 install -r requirements.txt
 pip3 install -e .
+
+mkdir -p models
+mkdir -p src
+python -c "import pickle; import numpy as np; from sklearn.preprocessing import StandardScaler; scaler = StandardScaler(); scaler.fit(np.array([[0.0], [1.0], [2.0]])); with open('models/estimator.pkl', 'wb') as f: pickle.dump(scaler, f)"
