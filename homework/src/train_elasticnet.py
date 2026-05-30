ca00938a39b90970f8a4d02895613fd957eb0036
dbermudeza <dbermudeza@unal.edu.co>
2026-05-30 08:37:37 -0500

    intento 1

--- homework/src/train_elasticnet.py
+++ homework/src/train_elasticnet.py
@@ -11,23 +11,12 @@
 # importacion de librerias
 from sklearn.linear_model import ElasticNet
 
-from ._internals.calculate_metrics import calculate_metrics
-from ._internals.prepare_data import prepare_data
-from ._internals.print_metrics import print_metrics
-from ._internals.save_model import save_model
+from ._internals.run_experiment import run_experiment
 
-x_train, x_test, y_train, y_test = prepare_data()
-
-# entrenar el modelo
-estimator = ElasticNet(alpha=0.5, l1_ratio=0.5, random_state=12345)
-estimator.fit(x_train, y_train)
-save_model(estimator)
-
-print()
-print(estimator, ":", sep="")
-
-mse, mae, r2 = calculate_metrics(x_train, y_train, estimator)
-print_metrics(mse, mae, r2, title="Metricas de entrenamiento:")
-
-mse, mae, r2 = calculate_metrics(x_test, y_test, estimator)
-print_metrics(mse, mae, r2, title="Metricas de testing:")
+run_experiment(
+    estimator=ElasticNet(
+        alpha=0.5,
+        l1_ratio=0.5,
+        random_state=12345,
+    )
+)
